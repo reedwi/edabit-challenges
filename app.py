4556809a0b025a8bd1177cfe83d9e92e1d17a480
reedwi <65463278+reedwi@users.noreply.github.com>
2020-12-12 14:10:25 -0500

    again

--- app.py
+++ app.py
@@ -460,3 +460,231 @@ def unique_sort(lst):
     new_lst = list(dict.fromkeys(lst))
     new_lst.sort()
     return new_lst
+
+# Create the function that takes a list of dictionaries and returns the sum of people's budgets.
+def get_budgets(lst):
+    budgets = [x['budget'] for x in lst]
+    return sum(budgets)
+
+# Create a function that returns the list of numbers from a given range. But for multiples of three, return 
+# “Eda” instead of the number and for the multiples of five, return “Bit”. For numbers which are multiples of both three and five, return “EdaBit”.
+def eda_bit(start, end):
+    lst = []
+    for val in range(start, end+1):
+        if val % 3 == 0 and val % 5 == 0:
+            lst.append('EdaBit')
+        elif val % 3 == 0:
+            lst.append('Eda')
+        elif val % 5 == 0:
+            lst.append('Bit')
+        else:
+            lst.append(val)
+    return lst
+
+# Create a function that takes the height and radius of a cone as arguments and returns the volume of the cone rounded to the nearest hundredth. 
+# See the resources tab for the formula.
+def cone_volume(h, r):
+    return round(((1/3) * math.pi * (r**2) * h), 2)
+
+# The Code tab has code which attempts to add a clone of a list to itself. There is no error message, but the results are not as intended. Can you fix the code?
+def clone(lst):
+    lst.append(lst[:])
+    return lst
+
+# Create a function that returns True if the given string has any of the following:
+
+# Only letters and no numbers.
+# Only numbers and no letters.
+# If a string has both numbers and letters, or contains characters which don't fit into any category, return False
+def alphanumeric_restriction(s):
+    if s.isdigit() == True:
+        return True
+    elif s.isalpha() == True:
+        return True
+    else:
+        return False
+
+# Additional spaces have been added to a sentence. Return the correct sentence by removing them. All words should be separated by one space, and there should 
+# be no spaces at the beginning or end of the sentence.
+def correct_spacing(sentence):
+    sentence.strip()
+    return " ".join(sentence.split())
+
+# Create a function that takes a string as input and capitalizes a letter if its ASCII code is even and returns its lower case version if its ASCII code is odd.
+def ascii_capitalize(txt):
+    new = []
+    for val in txt:
+        if ord(val) % 2 == 0:
+            new.append(val.upper())
+        else:
+            new.append(val.lower())
+    return ''.join(new)
+
+# Check the principles of minimalist code in the intro to the first challenge.
+
+# In the Code tab you will find a code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+# Write a function that returns the strings:
+
+# "both" if both given booleans a and b are True.
+# "first" if only a is True.
+# "second" if only b is True .
+# "neither" if both a and b are False.
+def are_true(a, b):
+    if a == True and b == True:
+        return 'both' 
+    elif a == True and b == False:
+        return 'first' 
+    elif a == False and b == True:
+        return 'second' 
+    else: return 'neither' 
+
+# A set is a collection of unique items. A set can be formed from a list from removing all duplicate items.
+def setify(lst):
+    new = list(set(lst))
+    new.sort()
+    return new
+
+# Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
+def num_layers(n):
+    val = 0.5*2**n/1000
+    return '{}m'.format(val)
+
+# Create a function that takes a number as an argument and returns True or False depending on whether the number is 
+# symmetrical or not. A number is symmetrical when it is the same as its reverse.
+def is_symmetrical(num):
+    newstr = str(num)
+    new = newstr[::-1]
+    if newstr == new:
+        return True
+    else: return False
+
+# A snail goes up the stairs. Every step, he must go up the step, then go across to the next step. He wants to reach the top of the tower.
+
+# Write a function that returns the distance the snail must travel to the top of the tower given the height and length of each step and the height of the tower.
+def total_distance(height, length, tower):
+    return round(tower/height * (height+length), 1)
+
+# A salesman has a number of cities to visit. They want to calculate the total number of possible paths they could take, visiting each city once before returning home. 
+# Return the total number of possible paths a salesman can travel, given n cities.
+
+# If we have cities A, B and C, possible paths would be:
+def paths(n):
+    return math.factorial(n)
+
+# Count the amount of ones in the binary representation of an integer. For example, since 12 is 1100 in binary, the return value should be 2.
+def count_ones(num):
+    binary = bin(num)
+    return int(str(binary).count('1'))
+
+# Write a function that retrieves the last n elements from a list.
+def last(a, n):
+    if n == 0:
+        return []
+    elif len(a) + 1 < n or a == []:
+        return 'invalid'
+    else:
+        return a[-n:]
+
+# Create a function that takes two strings and returns either True or False depending on whether they're anagrams or not.
+def is_anagram(s1, s2):
+    sort1 = sorted(s1.lower())
+    print(sort1)
+    sort2 = sorted(s2.lower())
+    print(sort2)
+    if sort1 == sort2:
+        return True
+    else:
+        return False
+
+# Create a function that moves all capital letters to the front of a word.
+def cap_to_front(s):
+    upper = [char for char in s if char.isupper()]
+    lower = [char for char in s if char.islower()]
+    return ''.join(upper)+''.join(lower)
+
+# Create a function to calculate how many characters in total are needed to make up the shape. 
+# You will be given a list of strings which make up a shape in the compiler (i.e. a square, a rectangle or a line).
+def count_characters(lst):
+    return len([x for val in lst for x in val])
+    return sum(len(i) for i in lst)
+
+# Create a function that takes a single string as argument and returns an ordered list containing the indices of all capital letters in the string.
+def index_of_caps(word):
+    return [word.index(char) for char in word if char.isupper()]
+
+# Using list comprehensions, create a function that finds all even numbers from 1 to the given number.
+def find_even_nums(num):
+    return [n for n in range(1, num+1) if n % 2 == 0]
+
+# Create a function that returns a base-2 (binary) representation of a base-10 (decimal) string number. 
+# To convert is simple: ((2) means base-2 and (10) means base-10) 010101001(2) = 1 + 8 + 32 + 128.
+
+# Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the value, 
+# value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
+def binary(d):
+	return bin(d)[2:]
+
+# Create a function that counts the number of times a particular letter shows up in the word search.
+def letter_counter(lst, letter):
+    return sum([row.count(letter) for row in lst])
+
+# Create a function that takes a variable number of arguments, each argument representing the number of items in a group, 
+# and returns the number of permutations (combinations) of items that you could get by taking one item from each group.
+def combinations(*items):
+    result = 1
+    for x in items:
+        if x == 0:
+            result = result*1
+        else:
+            result = result*x
+    return result
+
+# Return the sum of all items in a list, where each item is multiplied by its index (zero-based). For empty lists, return 0.
+def index_multiplier(lst):
+    return sum([i*x for i, x in enumerate(lst)])
+
+# Create a function that converts two lists of x- and y- coordinates into a list of (x, y) coordinates.
+def convert_cartesian(x, y):
+    return [[coordx, y[i]] for i, coordx in enumerate(x)]
+
+# A museum wants to get rid of some exhibitions. Katya, the interior architect, comes up with a plan to remove the most boring exhibitions. 
+# She gives them a rating, and removes the one with the lowest rating. Just as she finishes rating the exhibitions, she's called off to an important meeting. 
+# She asks you to write a program that tells her the ratings of the items after the lowest one is removed.
+
+# Create a function that takes a list of integers and removes the smallest value.
+def remove_smallest(lst):
+    if lst:
+        srtd = sorted(lst)
+        lst.remove(srtd[0])
+    return lst
+
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. 
+# Your task is to create a function that takes a string and returns True if the PIN is valid and False if it's not.
+def is_valid_PIN(pin):
+    if len(pin) in (4, 6) and pin.isdigit():
+        return True
+    else: return False
+
+# A financial institution provides professional services to banks and claims charges from the customers based on the number of man-days provided. 
+# Internally, it has set a scheme to motivate and reward staff to meet and exceed targeted billable utilization and revenues by paying a bonus for 
+# each day claimed from customers in excess of a threshold target.
+
+# This quarterly scheme is calculated with a threshold target of 32 days per quarter, and the incentive payment for each billable day in excess of such 
+# threshold target is shown as follows:
+# Please note that incentive payment is calculated progressively. As an example, if an employee reached total billable days of 45 in a quarter, 
+# his/her incentive payment is computed as follows:
+def bonus(days):
+    if days <= 32:
+        return 0
+    elif 40 >= days > 32:
+        return (days - 32) * 325
+    elif 48 >= days > 40:
+        return (8 * 325) + (550 * (days-40))
+    else:
+        return (8 * 325) +  (8 * 550) + (600 * (days-48))
+
+# Given a list of scrabble tiles (as dictionaries), create a function that outputs the maximum possible score a player can achieve by summing up the total number of points 
+# for all the tiles in their hand. Each hand contains 7 scrabble tiles.
+def maximum_score(tile_hand):
+    return sum([x['score'] for x in tile_hand])
